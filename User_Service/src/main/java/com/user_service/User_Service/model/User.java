e86b70314fa0119270ec328f94f5d8dda255d199
Pavan Reddy <alwayspawan148@gmail.com>
2025-04-13 09:22:30 +0530

    user service

---
+++ User_Service/src/main/java/com/user_service/User_Service/model/User.java
@@ -0,0 +1,19 @@
+package com.user_service.User_Service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    private String email;
+    private String password;
+}
---
+++ User_Service/src/main/java/com/user_service/User_Service/service/UserService.java
@@ -0,0 +1,31 @@
+package com.user_service.User_Service.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.user_service.User_Service.model.User;
+import com.user_service.User_Service.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
+
